42d2e8ed3975657071ca43a1241d99bc08325926
karma-may <144169780+karma-may@users.noreply.github.com>
2023-10-23 23:11:47 -0600

    Hw 8

---
+++ Homework 8/Untitled-1.js
@@ -0,0 +1,18 @@
+
+
+<!DOCTYPE html> 
+<html lang="en"> 
+  
+<body> 
+    <p>Required Attribute</p> 
+  
+    <form> 
+        <label>Name:</label> 
+        <input type="text" required /> 
+        <label>Email:</label> 
+        <input type="email" required /> 
+        <button style="margin-top: 5px">Submit</button> 
+    </form> 
+</body> 
+  
+</html> 
